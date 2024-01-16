451f04d143a51f065f70954331203df2f170220f
harshit71 <harshit.raj@itential.com>
2024-01-16 13:35:50 +0530

    LoginPage Component design added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { ConfigProvider } from "antd";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+import "antd/dist/reset.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#F65F42",
+          colorLink: "#F65F42",
+        },
+      }}
+    >
+      <RouterProvider router={router} />
+    </ConfigProvider>
   </React.StrictMode>
 );
